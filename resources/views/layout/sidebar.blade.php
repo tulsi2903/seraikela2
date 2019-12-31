69add0043939290aa062b8e1ec784a2747af56dc
tulsi2903 <55585978+tulsi2903@users.noreply.github.com>
2019-12-31 10:14:33 +0530

    side_bar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -79,6 +79,7 @@
 							<li class="nav-item"><a href="{{url('scheme-asset')}}"><i class="fas fa-users-cog"></i><p>Scheme Asset</p></a></li>
 
 			                <li class="nav-item"><a href="{{url('scheme-geo-target')}}"><i class="fas fa-receipt"></i><p>Scheme Geo Target</p></a></li>
+							<li class="nav-item"><a href="{{url('scheme-geo-target/pmayg')}}"><i class="fas fa-receipt"></i><p>PMAYG Target</p></a></li>
 			                <li class="nav-item"><a href="{{url('scheme-performance/add')}}"><i class="fas fa-receipt"></i><p>Scheme Performance</p></a></li>
 						</ul>
 					</div>
