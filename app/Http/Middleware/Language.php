c1d3636af95f1ec1a1aaca5d14660364f05a7c4e
sachinkpcode <56529925+sachinkpcode@users.noreply.github.com>
2020-02-11 18:49:23 +0530

    language

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -4,7 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Languages;
-use Auth;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -21,10 +22,12 @@ class Language
     public function handle($request, Closure $next)
     {
         
-        // $lang_id = Auth::user()->language;
+        $lang_id = Auth::user()->language;
+        if($lang_id!=1&&$lang_id!=2){
+            $lang_id = 1;
+        }
                
-        $lang = Languages::where('status',1)->first(); 
-        
+        $lang = Languages::find($lang_id); 
         $final_lang=json_decode($lang->languagePhrases);
         \Illuminate\Support\Facades\View::share('phrase',$final_lang);
         return $next($request);
