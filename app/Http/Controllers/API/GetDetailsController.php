17799331eacbffa69d1aca51044c2fdb03588e49
sachinkpcode <56529925+sachinkpcode@users.noreply.github.com>
2020-07-02 14:16:37 +0530

    Update api ->  block according to panchayat

--- app/Http/Controllers/API/GetDetailsController.php
+++ app/Http/Controllers/API/GetDetailsController.php
@@ -9,6 +9,7 @@ use App\Year;
 use App\GeoStructure;
 use App\Asset;
 use App\SchemeAsset;
+use Illuminate\Support\Facades\Auth;
 
 class GetDetailsController extends Controller
 {
@@ -36,6 +37,9 @@ class GetDetailsController extends Controller
 
     // get block details
     public function get_blocks(Request $request){
+        $user_designation = Auth::user()->userRole;
+        $geo_ids = [];
+
         if($request->block_id){
             $datas = GeoStructure::where('geo_id', $request->block_id)->select('geo_id','geo_name')->first();
         }
@@ -43,7 +47,32 @@ class GetDetailsController extends Controller
             $datas = GeoStructure::where('sd_id', $request->subdivision_id)->where('level_id', 3)->select('geo_id','geo_name')->get();
         }
         else{
-            $datas = GeoStructure::where('level_id', 3)->select('geo_id','geo_name')->get();
+            // getting block ids for that designation/ assigned
+            if ($user_designation == 1) // dc
+            {
+                $geo_ids = GeoStructure::where('level_id', 3)->pluck('geo_id'); // all block_ids
+            } 
+            else if ($user_designation  == 2) { // sdo
+                $subdivision_id_tmp = GeoStructure::where('officer_id', Auth::user()->id)->where('level_id', '2')->first();
+                if ($subdivision_id_tmp) { // if user is assigned to some subdivision
+                    $geo_ids = GeoStructure::where('sd_id', $subdivision_id_tmp->geo_id)->pluck('geo_id'); // block_ids, panchayat_ids
+                }
+            } 
+            else if ($user_designation == 3) { // bdo
+                $block_id_tmp = GeoStructure::where('officer_id', Auth::user()->id)->first();
+                if ($block_id_tmp) { // if user is assigned to some block
+                    $geo_ids = GeoStructure::where('officer_id', Auth::user()->id)->pluck('geo_id'); // block_ids, panchayat_ids
+                }
+                
+                // return ["geo_ids"=>$block_id_tmp];
+            } 
+            else if ($user_designation == 4) { //po
+                $panchayat_id_tmp = GeoStructure::where('officer_id', Auth::user()->id)->first();
+                if ($panchayat_id_tmp) { // if user is assigned to some panchayats
+                    $geo_ids = GeoStructure::where('geo_id', $panchayat_id_tmp->bl_id)->pluck('geo_id'); // block_ids, panchayat_ids
+                }
+            }
+            $datas = GeoStructure::select('geo_id','geo_name')->where('level_id', 3)->whereIn('geo_id', $geo_ids)->get();
         }
 
         // return after validate
